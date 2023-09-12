fc2ae188d3574942938e567d4e3842f92c59adff
delana <raihandelana@gmail.com>
2023-09-12 20:03:24 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -6,10 +6,12 @@ class Mahasiswa {
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    int umur= DateTime.now().year-tahunLahir;
+    print("Perkenalkan, nama saya $nama dengan NIM $nim dan saya berumur $umur tahun");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  var mhs = Mahasiswa("H1D021021", "Annisa Raihan Delana", 2002);
+  mhs.perkenalan();
 }
